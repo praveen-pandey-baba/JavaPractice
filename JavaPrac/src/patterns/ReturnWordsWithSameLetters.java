c277092be6863bd923cce51f5fc7457120eedc55
aurojit-das <aurojit-das@USDLSEVDIX00833.mercer.com>
2020-03-02 19:43:42 +0530

    FirstCommit

---
+++ JavaPrac/src/patterns/ReturnWordsWithSameLetters.java
@@ -0,0 +1,12 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class ReturnWordsWithSameLetters {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
---
+++ JavaPrac/src/patterns/UltaPyramid.java
@@ -0,0 +1,32 @@
+package patterns;
+
+public class UltaPyramid {
+	
+	public static void main(String[] args) {
+		int j;
+		for(int i=0;i<5;i++) {
+			for(j=0;j<4-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int k =0;k<=i;k++) {
+				System.out.print("*");
+			}			
+			System.out.println();			
+		}
+		
+		for(int i=4;i>0;i--) {
+			for(j=0;j<=4-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int k =0;k<i;k++) {
+				System.out.print("*");
+			}			
+			System.out.println();			
+		}
+		
+		
+	}
+
+}
