ece6192fc344c6d890b442db083eee0da779ca43
aurojit-das <aurojit-das@USDLSEVDIX00833.mercer.com>
2020-03-02 20:12:22 +0530

    AddingSquareCode...

---
+++ JavaPrac/src/patterns/SuqareAndStarInMiddle.java
@@ -0,0 +1,25 @@
+package patterns;
+
+public class SuqareAndStarInMiddle {
+
+       public static void main(String[] args) {
+              
+              for(int i=0;i<=4;i++) {
+                     for(int j=0;j<=4;j++) {
+                     if(i==0||i==4) {
+                           System.out.print("*");
+                     }else  if(i<=3&&i>=1){
+                           if(j==0||j==4) {
+                           System.out.print("*");
+                           }else if(i==2&&j==2){
+                                  System.out.print("*");
+                           }else{
+                                  System.out.print(" ");    
+                           }
+                           
+                     }                    
+              }
+                     System.out.println();
+       }
+}
+}
