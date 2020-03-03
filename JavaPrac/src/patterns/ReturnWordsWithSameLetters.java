bc24b9f59f3ba97d3014c639357dc3879a00bddd
aurojit-das <aurojit-das@USDLSEVDIX00833.mercer.com>
2020-03-03 12:02:13 +0530

    Adding Return Word with same Letters...

--- JavaPrac/src/patterns/ReturnWordsWithSameLetters.java
+++ JavaPrac/src/patterns/ReturnWordsWithSameLetters.java
@@ -3,9 +3,32 @@ package patterns;
 import java.util.Scanner;
 
 public class ReturnWordsWithSameLetters {
+	
+	
+	public static boolean chk(String s) {
+		 int n = s.length(); 
+		    for (int i = 1; i < n; i++) {
+		    	 if (s.charAt(i) != s.charAt(0)) 
+			            return false;
+		    }
+		        
+		          
+		    return true; 
+	}
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string: ");		
+		String str = sc.nextLine();
+		String[] words = str.split(" ");
+		for(int i = 0;i<words.length;i++) {
+			  if (chk(words[i])) { 
+			        System.out.println(words[i]); 
+			  }			    
+		}
+		sc.close();
+		
 
 	}
 
